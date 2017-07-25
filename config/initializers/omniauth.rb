6b57e0bfbe7d45e306ca557d7f6d085d81bd0bad
Andrew Rogers <andrewprogers@gmail.com>
2017-07-25 18:56:42 -0400

    fix indentation

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,9 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'],
-  scope: 'profile',
-  image_aspect_ratio: 'square',
-  image_size: 48,
-  access_type: 'online',
-  verify_iss: false
+    scope: 'profile',
+    image_aspect_ratio: 'square',
+    image_size: 48,
+    access_type: 'online',
+    verify_iss: false
 end
