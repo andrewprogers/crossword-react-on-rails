6dc54a0eadd3480635b1485183e1a97aa357459f
Andrew Rogers <andrewprogers@gmail.com>
2017-07-31 11:52:08 -0400

    Remove static page for puzzle

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
-  get '/puzzle' => 'static_pages#puzzle'
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
