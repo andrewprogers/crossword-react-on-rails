64b24ce1fb544d6c331e2dc1dd039dcf3517baca
Andrew Rogers <andrewprogers@gmail.com>
2017-08-07 16:38:57 -0400

    Restyle menu buttons

--- app/javascript/react/src/components/MenuButton.js
+++ app/javascript/react/src/components/MenuButton.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const MenuButton = props => {
-  let letter = props.name.charAt(0)
   return(
     <div className="menu-button" onClick={props.onClick}>
-      <div className="menu-letter">{letter}</div>
       <div className='menu-name'>{props.name}</div>
     </div>
   )
